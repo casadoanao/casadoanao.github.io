9da726ae7d20c85bb7a43c26dc5c39d12ad815b3
baianosaoik123 <55710800+baianosaoik123@users.noreply.github.com>
2021-05-13 07:23:35 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,26 @@
-const upperLinks = document.querySelectorAll('a.upperLink')
-const actualPage = window.location.href
+(function() {
+	const upperLinks = document.querySelectorAll('a.upperLink')
+	const currentPage = window.location.href
 
-function addElementClass(el, classToAdd) {
-	return function () {
-		el.classList.add(classToAdd)
-	}
-}
-
-function removeElementClass(el, classToRemove) {
-	return function () {
-		el.classList.remove(classToRemove)
-	}
-}
-
-for (const link in upperLinks) {
-	const referredKey = upperLinks[link]
-	referredKey.onmouseenter = addElementClass(referredKey, 'active')
-	referredKey.onmouseout = removeElementClass(referredKey, 'active')
-
-	if (referredKey.href !== undefined) {
-		if (referredKey.href === actualPage) {
-			referredKey.title = "Você está aqui"
+	function addElementClass(el, classToAdd) {
+		return () => {
+			el.classList.add(classToAdd)
 		}
 	}
-}
+
+	function removeElementClass(el, classToRemove) {
+		return () => {
+			el.classList.remove(classToRemove)
+		}
+	}
+
+	for (const link in upperLinks) {
+		const element = upperLinks[link]
+		element.onmouseenter = addElementClass(element, 'active')
+		element.onmouseout = removeElementClass(element, 'active')
+
+		if (element.href === currentPage) {
+			element.title = "Você está aqui"
+		}
+	}
+})()
